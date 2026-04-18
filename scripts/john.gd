bf795510a5e633ad5c6ca385c40551dcd67d184b
Portalyst <steevex1977@gmail.com>
2026-04-18 12:36:59 +0500

    Texture update

--- scripts/john.gd
+++ scripts/john.gd
@@ -1,26 +1,20 @@
 extends CharacterBody2D
 
-
 var speed = 40.0
 
-
 func _physics_process(delta: float) -> void:
-	var direction := Vector2(Input.get_axis("ui_left", "ui_right"), Input.get_axis("ui_up", "ui_down"))
+	var direction := Vector2(Input.get_axis("left", "right"), Input.get_axis("up", "down"))
 	var anim_is_playing : bool = false
-	if Input.is_action_pressed("ui_down") and anim_is_playing == false:
-		#direction = Vector2(0, 1)
+	if Input.is_action_pressed("down") and anim_is_playing == false:
 		$AnimatedSprite2D.play("run_d")
 		anim_is_playing = true
-	if Input.is_action_pressed("ui_up") and anim_is_playing == false:
-		#direction = Vector2(0, -1)
+	if Input.is_action_pressed("up") and anim_is_playing == false:
 		$AnimatedSprite2D.play("run_u")
 		anim_is_playing = true
-	if Input.is_action_pressed("ui_left") and anim_is_playing == false:
-		#direction = Vector2(-1, 0)
+	if Input.is_action_pressed("left") and anim_is_playing == false:
 		anim_is_playing = true
 		$AnimatedSprite2D.play("run_l")
-	if Input.is_action_pressed("ui_right") and anim_is_playing == false:
-		#direction = Vector2(1, 0)
+	if Input.is_action_pressed("right") and anim_is_playing == false:
 		$AnimatedSprite2D.play("run_r")
 		anim_is_playing = true
 	if direction:
